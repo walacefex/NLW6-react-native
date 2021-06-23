6e436f7fe51e2994ba6e1d7bac401d12f34162ed
Walacefex <walacefex01@gmail.com>
2021-06-23 14:13:57 -0300

    Criando a organização do projeto com as pastas, componenets, global aonde se encontra o estilo global, criando pasta screens para as telas e implementando a pasta SignIn

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+export const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: theme.colors.background
+  },
+  image: {
+  width: '100%',
+  height: 360, 
+  },
+  content: {
+    marginTop:-40,
+    paddingHorizontal: 50
+  },
+  title: {
+    color: theme.colors.headign,
+    textAlign: 'center',
+    fontSize: 40,
+    marginBottom: 16,
+  },
+  subtitle: {
+    color: theme.colors.headign,
+    fontSize: 15,
+    textAlign: 'center',
+    marginBottom:64
+   }
+})
